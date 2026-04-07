74c0b786b0855c705ed0ec699d2d5da9ffe13932
Jaakko Kaisanlahti <jaakko.kaisanlahti@erpg.fi>
2026-04-07 21:14:41 +0300

    Expand and fix unused wildcard imports in CharacterData.java and FooterViewModel.java after package reorganization.

--- src/main/java/com/vibethema/viewmodel/footer/FooterViewModel.java
+++ src/main/java/com/vibethema/viewmodel/footer/FooterViewModel.java
@@ -1,12 +1,8 @@
 package com.vibethema.viewmodel.footer;
 
 import com.vibethema.model.*;
-import com.vibethema.model.combat.*;
-import com.vibethema.model.equipment.*;
 import com.vibethema.model.logic.*;
-import com.vibethema.model.mystic.*;
 import com.vibethema.model.progression.*;
-import com.vibethema.model.social.*;
 import com.vibethema.model.traits.*;
 import de.saxsys.mvvmfx.ViewModel;
 import javafx.beans.property.BooleanProperty;
